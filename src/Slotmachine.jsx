cd466ffa168a0e4fd6bcf354df79ff0240924a6b
Dylan <dylanabozarth@gmail.com>
2020-08-30 21:29:56 -0700

    added spin and bootstrap

--- src/Slotmachine.jsx
+++ src/Slotmachine.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 
+import { Button, Container } from 'react-bootstrap';
 class SlotMachine extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        first: ["Bill Gates", "Donald Trump", "Obama", "The Lizard People", "The Illuminatnti", "The Freemasons", "Mask Manufactors", "Aliens", "Elon Musk", "Jeff Besos", "CNN", "The W.H.O.", "Alex Jones"],
-        second: ["created Corona Virus in order ", "released Covid-19 ", "created Covid-19", "bought Covid-19 from Wuhan", "created Covid-19", "ate bat soup"],
-        third: ["to destroy America", "to make billions", "to turn the frogs gay", "to prepare for covid-20", " to depopulate the Earth" ,"to colonize Mars", "to put a microchip in everyone"]
+        first: ["Bill Gates", "Donald Trump", "Obama", "The Lizard People", "The Illuminatnti", "The Freemasons", "Mask Manufactors", "Aliens", "Jeff Besos", "China", "The W.H.O.", "Alex Jones"],
+        second: ["created Corona Virus  ", "released Covid-19 ", "created Covid-19", "bought Covid-19 from Wuhan", "created Covid-19", "ate bat soup", "Made 5G", "canceled football", "bought covid-19 from aliens"],
+        third: ["to destroy America", "to make billions", "to turn the frogs gay", "to prepare for covid-20", " to depopulate the Earth" ,"to colonize Mars", "to put a microchip in everyone", "to force everyone to use 5G", "to make everyone stay home"]
       };
      
            
     
     }
+  
     randomnum()  {
-        let num = Math.floor(Math.random() * 7) + 0
+        let num = Math.floor(Math.random() * 9) + 0
         console.log(num)
         return num
       }
@@ -24,7 +26,7 @@ componentDidMount() {
     
   render() {
     return (
-      <div className="textcontainer">
+      <Container className="textcontainer">
         <div className="firstline">
 {this.state.first[this.randomnum()]}
         </div>
@@ -36,7 +38,8 @@ componentDidMount() {
         <div className="thirdline">
        {this.state.third[this.randomnum()]}
         </div>
-      </div>
+        <Button onClick={() => window.location.reload(false)}>Click to reload!</Button>
+      </Container>
     );
   }
 }
