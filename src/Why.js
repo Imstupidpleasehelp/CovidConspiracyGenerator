0883bbb1fa74f58ffae0451934b0a25afc0e725a
Dylan <dylanabozarth@gmail.com>
2020-09-01 14:58:03 -0700

    added description

--- src/Why.js
+++ src/Why.js
@@ -4,7 +4,7 @@ import Collapsible from 'react-collapsible';
 
 export const Why = () => (
     <Collapsible className="dropdown" openedClassName="opened" trigger="Start here" triggerStyle={{color: 'red'}}>
-    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <p>Working at a grocery store during the covid crisis I was on the front lines of not only being exposed to the virus, but being exposed to the latest conspiracy theories as well. So I made this, to illustrate the things that I have heard from customers and coworkers over the past few months.</p>
     
   </Collapsible>
 )
