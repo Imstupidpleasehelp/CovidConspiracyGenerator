7229eed5a014856176b2f72474da4f3c2b46fd04
Dylan <dylanabozarth@gmail.com>
2020-09-01 22:33:37 -0700

    incomplete animations

--- src/Slotmachine.jsx
+++ src/Slotmachine.jsx
@@ -1,44 +1,101 @@
 import React, { Component } from "react";
-import { motion } from "framer-motion"
-import { Button, Container } from 'react-bootstrap';
+import { motion } from "framer-motion";
+import { Button, Container } from "react-bootstrap";
+const changepage = {
+  in: {
+    opacity: 1,
+  },
+  out: {
+    opacity: 0,
+  },
+};
+const pagetransition = {
+  duration: 1.5,
+};
 class SlotMachine extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        first: ["Bill Gates", "Donald Trump", "Obama", "The Lizard People", "The Illuminatnti", "The Freemasons", "Mask Manufactors", "Aliens", "Jeff Besos", "China", "The W.H.O.", "Alex Jones"],
-        second: ["created Corona Virus  ", "released Covid-19 ", "created Covid-19", "bought Covid-19 from Wuhan", "created Covid-19", "ate bat soup", "Made 5G", "cancelled football", "bought covid-19 from aliens"],
-        third: ["to destroy America", "to make billions", "to turn the frogs gay", "to prepare for covid-20", " to depopulate the Earth" ,"to colonize Mars", "to put a microchip in everyone", "to force everyone to use 5G", "to make everyone stay home"]
-      };
-     
-           
-    
-    }
-    
-    randomnum()  {
-        let num = Math.floor(Math.random() * 9) + 0
-        console.log(num)
-        return num
-      }
-componentDidMount() {
-    this.randomnum();
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      first: [
+        "Bill Gates",
+        "Donald Trump",
+        "Obama",
+        "The Lizard People",
+        "The Illuminatnti",
+        "The Freemasons",
+        "Mask Manufactors",
+        "Aliens",
+        "Jeff Besos",
+        "China",
+        "The W.H.O.",
+        "Alex Jones",
+      ],
+      second: [
+        "created Corona Virus  ",
+        "released Covid-19 ",
+        "created Covid-19",
+        "bought Covid-19 from Wuhan",
+        "created Covid-19",
+        "ate bat soup",
+        "Made 5G",
+        "cancelled football",
+        "bought covid-19 from aliens",
+      ],
+      third: [
+        "to destroy America",
+        "to make billions",
+        "to turn the frogs gay",
+        "to prepare for covid-20",
+        " to depopulate the Earth",
+        "to colonize Mars",
+        "to put a microchip in everyone",
+        "to force everyone to use 5G",
+        "to make everyone stay home",
+      ],
+    };
+  }
 
-    
+  randomnum() {
+    let num = Math.floor(Math.random() * 9) + 0;
+    console.log(num);
+    return num;
+  }
+  clearAlert() {
+    clearTimeout();
+  }
+  componentDidMount() {
+    this.randomnum();
+  }
+
   render() {
     return (
-      <Container className="textcontainer">
-        <div className="firstline">
-{this.state.first[this.randomnum()]}
-        </div>
+      <Container>
+        <motion.Container
+          initial="out"
+          animate="in"
+          exit="out"
+          variants={changepage}
+          transition={pagetransition}
+          className="textcontainer"
+        >
+          <div className="firstline">{this.state.first[this.randomnum()]}</div>
 
-        <div className="secondline">
-         {this.state.second[this.randomnum()]}
-        </div>
+          <div className="secondline">
+            {this.state.second[this.randomnum()]}
+          </div>
 
-        <div className="thirdline">
-       {this.state.third[this.randomnum()]}
-        </div>
-        <Button onClick={setTimeout(() => window.location.reload(false), 4000), clearTimeout()}>Click to reload!</Button>
+          <div className="thirdline">{this.state.third[this.randomnum()]}</div>
+        </motion.Container>
+        <Button className="reloadbutton"
+          onClick={setTimeout(
+            () => window.location.reload(false),
+            4000,
+            clearTimeout()
+            
+          )}
+        >
+          Click to reload!
+        </Button>
       </Container>
     );
   }
